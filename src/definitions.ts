b5e101f103801c6362fe7513a96d657695de3cae
Sebastián Di Lauro <jsdilauro@gmail.com>
2024-08-16 14:52:00 -0300

    complete electricidad.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,4 @@
+import { engine, Schemas } from "@dcl/sdk/ecs"
 import { type Vector3, type Quaternion } from "@dcl/sdk/math"
 
 export type BlenderTransform = {
@@ -5,3 +6,20 @@ export type BlenderTransform = {
     rotation:Quaternion,
     scale:Vector3
 }
+
+export const ElectricidadComponent = engine.defineComponent(
+	"electricidad",
+	{
+		entity_frames:  Schemas.Array(Schemas.Entity),
+    frame: Schemas.Int,
+    base_transform: Schemas.Map({
+      position: Schemas.Vector3,
+      scale: Schemas.Vector3,
+      parent: Schemas.Entity,
+      rotation: Schemas.Quaternion			
+    }),
+    gltf_frames:  Schemas.Array(Schemas.String),
+    looping: Schemas.Boolean,
+    playing: Schemas.Boolean
+  }
+)
--- src/electricidad.ts
+++ src/electricidad.ts
@@ -1,56 +1,6 @@
-import { engine, type Entity, GltfContainer, Schemas, Transform } from "@dcl/sdk/ecs"
+import { engine, type Entity, GltfContainer, Transform } from "@dcl/sdk/ecs"
+import { ElectricidadComponent } from "./definitions"
 
-
-
-export const ElectricidadComponent = engine.defineComponent(
-	"electricidad",
-	{
-		entity_frames:  Schemas.Array(Schemas.Entity),
-    frame: Schemas.Int,
-    base_transform: Schemas.Map({
-      position: Schemas.Vector3,
-      scale: Schemas.Vector3,
-      parent: Schemas.Entity,
-      rotation: Schemas.Quaternion			
-    }),
-    gltf_frames:  Schemas.Array(Schemas.String),
-    looping: Schemas.Boolean,
-    playing: Schemas.Boolean
-	})
-
-
-
-// SDK6
-// @Component('electricidad')
-// export class ElectricidadComponent {
-//   entity_frames: Entity[]
-//   frame: number
-//   base_transform: Transform
-//   gltf_frames: string[]
-//   looping: boolean
-//   playing: boolean
-//   constructor() {
-//     this.entity_frames = []
-//     this.frame = 0
-//     this.base_transform = new Transform()
-//     this.gltf_frames = []
-//     this.looping = true
-//     this.playing = true
-//   }
-
-//   playOnce() {
-//     this.frame = 0
-//     this.looping = false
-//     this.playing = true
-//   }
-// }
-
-
-// export class Electricidad extends Entity {
-//   constructor(electricidadPasilloFrames: string[]) {
-//     super()
-// const elect_component = new ElectricidadComponent()
-// this.addComponent(elect_component)
 
 export function createElectricidad(electricidadPasilloFrames:string[]):Entity{
   const electricidad = engine.addEntity()
@@ -66,46 +16,33 @@ export function createElectricidad(electricidadPasilloFrames:string[]):Entity{
   return electricidad
 }
 
-const electGroup = engine.getEntitiesWith(ElectricidadComponent)
-export class ElectricidadSystem implements ISystem {
-  timePass: number = 0
-  update(td: number) {
-    this.timePass += td
-    if (this.timePass < 0.1) {
-      return
-    }
-    this.timePass = 0
-    for (const elect of electGroup.entities) {
-      const elect_component = elect.getComponent(ElectricidadComponent)
-      if (!elect_component.playing) {
-        for (let n = 0; n < elect_component.entity_frames.length; n++) {
-          const frameTransform =
-            elect_component.entity_frames[n].getComponent(Transform)
-          frameTransform.position.y = -20
-          // engine.removeEntity(elect_component.entity_frames[n])
-        }
-        continue
-      }
-      //
-      for (let n = 0; n < elect_component.entity_frames.length; n++) {
+export function ElectricidadSystem(dt: number): void {
+  for (const [elect] of engine.getEntitiesWith(ElectricidadComponent)) {
+    const mutableElectComponent = ElectricidadComponent.getMutable(elect)
+    if (!mutableElectComponent.playing) {
+      for (let n = 0; n < mutableElectComponent.entity_frames.length; n++) {
         const frameTransform =
-          elect_component.entity_frames[n].getComponent(Transform)
-        if (n === elect_component.frame) {
-          frameTransform.position.y = 0
-          // engine.addEntity(elect_component.entity_frames[n])
-        } else {
-          frameTransform.position.y = -20
-          // engine.removeEntity(elect_component.entity_frames[n])
-        }
+        Transform.getMutable(mutableElectComponent.entity_frames[n])
+        frameTransform.position.y = -20
       }
-
-      elect_component.frame += 1
-      if (elect_component.frame >= elect_component.entity_frames.length) {
-        elect_component.frame = 0
-        if (!elect_component.looping) {
-          elect_component.playing = false
-        }
+      continue
+    }
+    for (let n = 0; n < mutableElectComponent.entity_frames.length; n++) {
+      const frameTransform =
+      Transform.getMutable(mutableElectComponent.entity_frames[n])
+      if (n === mutableElectComponent.frame) {
+        frameTransform.position.y = 0
+      } else {
+        frameTransform.position.y = -20
       }
     }
+    mutableElectComponent.frame += 1
+    if (mutableElectComponent.frame >= mutableElectComponent.entity_frames.length) {
+      mutableElectComponent.frame = 0
+        if (!mutableElectComponent.looping) {
+          mutableElectComponent.playing = false
+        }
+    }
+
   }
 }
